992ea84d7e4f012fe7bac1f1f356f7211e98f527
soulnafein <soulnafein@gmail.com>
2010-07-07 20:45:33 +0100

    Revert "Refactored twitter plugin"
    This reverts commit c4399617ae6237b6947ffc70988d13e344cf48ed.

--- public/javascripts/twitter.js
+++ public/javascripts/twitter.js
@@ -1,91 +1,30 @@
 (function($) {
   $.fn.twitter_feed = function(username) {
+    var feed_url = 'http://api.twitter.com/1/statuses/user_timeline.rss?id='+username;
+
+    var list = "";
 
     $(this).each(function() {
-      TwitterFeed.init(username, $(this));
+      list = $(this).find("ul:first");
+      load_feed();
     });
 
+    function load_feed() {
+      $.jGFeed(feed_url, display_feed, 5)
+    }
+
+    function display_feed(feed) {
+      if (!feed) {
+        alert("Error!");
+        return false;
+      }
+
+      var entries = feed.entries;
+      for(var i = 0; i < entries.length; i++) {
+        var entry = entries[i];
+        list.append("<li>" + entry.title + "</li>");
+      }
+      return true;
+    }
   }
 })(jQuery);
-
-var TwitterFeed = {};
-
-TwitterFeed.init = function(username, div) {
-  var model = TwitterFeed.model(username);
-  var view = TwitterFeed.view(div, model);
-  TwitterFeed.controller(model, view);
-  model.load();
-};
-
-TwitterFeed.controller = function(model, view) {
-  model.onLoadComplete = function() {
-    view.feedLoaded();
-  };
-
-  model.onError = function(){
-    view.errorOccurred();
-  }
-};
-
-TwitterFeed.model = function(username) {
-  var feed_url = 'http://api.twitter.com/1/statuses/user_timeline.rss?id=' + username;
-  var feed = '';
-
-  var self = {
-    onLoadComplete: function () {},
-    onError: function(){},
-
-    load: function () {
-      load_feed();
-    },
-    entries : function() {
-      return feed.entries;
-    }
-
-  };
-
-  function load_feed() {
-    $.jGFeed(feed_url, feed_loaded, 5);
-  }
-
-  function feed_loaded(_feed) {
-    if (!_feed) {
-      return self.onError();
-    }
-    feed = _feed;
-    self.onLoadComplete()
-  }
-
-  return self;
-};
-
-TwitterFeed.view = function(div, feed) {
-  var list = div.find("ul:first");
-
-  var self = {
-    feedLoaded: function() {
-      display_feed();
-    },
-    errorOccurred: function(){
-      raise_error();
-    }
-  };
-
-  function display_feed() {
-    var entries = feed.entries();
-    
-    for (var i = 0; i < entries.length; i++) {
-      var entry = entries[i];
-      list.append("<li>" + entry.title + "</li>");
-    }
-    return true;
-  }
-
-  function raise_error() {
-    alert("Error!");
-    return false;
-  }
-
-  return self;
-};
-
